6284cb8a50d8a40a40c9df79357ad315947204f7
Jin <cch1enn18@gmail.com>
2025-10-21 02:26:20 +0800

    update

--- TemperatureConversion/Form1.Designer.cs
+++ TemperatureConversion/Form1.Designer.cs
@@ -28,12 +28,243 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            groupBox1 = new GroupBox();
+            rbF2C = new RadioButton();
+            rbC2F = new RadioButton();
+            btn1 = new Button();
+            btn2 = new Button();
+            btn3 = new Button();
+            btn4 = new Button();
+            btn5 = new Button();
+            btn6 = new Button();
+            btn7 = new Button();
+            btn8 = new Button();
+            btn9 = new Button();
+            btn0 = new Button();
+            btnDot = new Button();
+            txtInput = new TextBox();
+            btnConvert = new Button();
+            btnClear = new Button();
+            label1 = new Label();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(rbF2C);
+            groupBox1.Controls.Add(rbC2F);
+            groupBox1.Location = new Point(29, 67);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(300, 150);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            // 
+            // rbF2C
+            // 
+            rbF2C.AutoSize = true;
+            rbF2C.Location = new Point(24, 96);
+            rbF2C.Name = "rbF2C";
+            rbF2C.Size = new Size(161, 27);
+            rbF2C.TabIndex = 1;
+            rbF2C.TabStop = true;
+            rbF2C.Text = "華氏轉換為攝氏";
+            rbF2C.UseVisualStyleBackColor = true;
+            // 
+            // rbC2F
+            // 
+            rbC2F.AutoSize = true;
+            rbC2F.Location = new Point(24, 45);
+            rbC2F.Name = "rbC2F";
+            rbC2F.Size = new Size(161, 27);
+            rbC2F.TabIndex = 0;
+            rbC2F.TabStop = true;
+            rbC2F.Text = "攝氏轉換為華氏";
+            rbC2F.UseVisualStyleBackColor = true;
+            // 
+            // btn1
+            // 
+            btn1.Location = new Point(358, 66);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(49, 45);
+            btn1.TabIndex = 1;
+            btn1.Text = "1";
+            btn1.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            btn2.Location = new Point(413, 67);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(49, 45);
+            btn2.TabIndex = 2;
+            btn2.Text = "2";
+            btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            btn3.Location = new Point(468, 67);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(49, 45);
+            btn3.TabIndex = 3;
+            btn3.Text = "3";
+            btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            btn4.Location = new Point(358, 117);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(49, 45);
+            btn4.TabIndex = 4;
+            btn4.Text = "4";
+            btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            btn5.Location = new Point(413, 118);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(49, 45);
+            btn5.TabIndex = 5;
+            btn5.Text = "5";
+            btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            btn6.Location = new Point(468, 118);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(49, 45);
+            btn6.TabIndex = 6;
+            btn6.Text = "6";
+            btn6.UseVisualStyleBackColor = true;
+            // 
+            // btn7
+            // 
+            btn7.Location = new Point(358, 168);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(49, 45);
+            btn7.TabIndex = 7;
+            btn7.Text = "7";
+            btn7.UseVisualStyleBackColor = true;
+            // 
+            // btn8
+            // 
+            btn8.Location = new Point(413, 169);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(49, 45);
+            btn8.TabIndex = 8;
+            btn8.Text = "8";
+            btn8.UseVisualStyleBackColor = true;
+            // 
+            // btn9
+            // 
+            btn9.Location = new Point(468, 169);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(49, 45);
+            btn9.TabIndex = 9;
+            btn9.Text = "9";
+            btn9.UseVisualStyleBackColor = true;
+            // 
+            // btn0
+            // 
+            btn0.Location = new Point(358, 219);
+            btn0.Name = "btn0";
+            btn0.Size = new Size(49, 45);
+            btn0.TabIndex = 10;
+            btn0.Text = "0";
+            btn0.UseVisualStyleBackColor = true;
+            // 
+            // btnDot
+            // 
+            btnDot.Location = new Point(413, 220);
+            btnDot.Name = "btnDot";
+            btnDot.Size = new Size(49, 45);
+            btnDot.TabIndex = 11;
+            btnDot.Text = ".";
+            btnDot.UseVisualStyleBackColor = true;
+            btnDot.Click += BtnDot_Click;
+            // 
+            // txtInput
+            // 
+            txtInput.Location = new Point(582, 109);
+            txtInput.Name = "txtInput";
+            txtInput.Size = new Size(271, 30);
+            txtInput.TabIndex = 12;
+            // 
+            // btnConvert
+            // 
+            btnConvert.Location = new Point(582, 163);
+            btnConvert.Name = "btnConvert";
+            btnConvert.Size = new Size(112, 34);
+            btnConvert.TabIndex = 13;
+            btnConvert.Text = "轉換";
+            btnConvert.UseVisualStyleBackColor = true;
+            btnConvert.Click += BtnConvert_Click;
+            // 
+            // btnClear
+            // 
+            btnClear.Location = new Point(700, 163);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(112, 34);
+            btnClear.TabIndex = 14;
+            btnClear.Text = "清除";
+            btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += BtnClear_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(582, 220);
+            label1.Name = "label1";
+            label1.Size = new Size(61, 23);
+            label1.TabIndex = 15;
+            label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 23F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(923, 450);
+            Controls.Add(label1);
+            Controls.Add(btnClear);
+            Controls.Add(btnConvert);
+            Controls.Add(txtInput);
+            Controls.Add(btnDot);
+            Controls.Add(btn0);
+            Controls.Add(btn9);
+            Controls.Add(btn8);
+            Controls.Add(btn7);
+            Controls.Add(btn6);
+            Controls.Add(btn5);
+            Controls.Add(btn4);
+            Controls.Add(btn3);
+            Controls.Add(btn2);
+            Controls.Add(btn1);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private GroupBox groupBox1;
+        private RadioButton rbF2C;
+        private RadioButton rbC2F;
+        private Button btn1;
+        private Button btn2;
+        private Button btn3;
+        private Button btn4;
+        private Button btn5;
+        private Button btn6;
+        private Button btn7;
+        private Button btn8;
+        private Button btn9;
+        private Button btn0;
+        private Button btnDot;
+        private TextBox txtInput;
+        private Button btnConvert;
+        private Button btnClear;
+        private Label label1;
     }
 }
--- TemperatureConversion/Form1.cs
+++ TemperatureConversion/Form1.cs
@@ -1,3 +1,5 @@
+using System.Drawing.Drawing2D;
+
 namespace TemperatureConversion
 {
     public partial class Form1 : Form
@@ -5,6 +7,120 @@ namespace TemperatureConversion
         public Form1()
         {
             InitializeComponent();
+
+            // 建立數字鍵盤共用事件
+            // 為什麼需要共用事件?
+            // 因數字按鈕的功能類似，都是將按下的數字顯示在輸入框中
+            // 如果每個按鈕都寫一個獨立的事件處理方法，會造成程式碼重複且難以維護
+            // 使用共用事件可以讓程式碼更簡潔且易於管理
+            // 共用事件的實現方式是將所有數字按鈕的 Click 事件都指向同一個事件處理方法
+            // 在事件處理方法中，可以使用 sender 參數來辨識是哪個按鈕被按下，然後取得該按鈕的文字內容
+
+            // 將所有數字按鈕放入一個陣列中，然後使用迴圈為每個按鈕訂閱相同的 Click 事件處理方法 digiBtns_Click
+            var digiBtns = new[] { btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9 };
+            foreach (var b in digiBtns) b.Click += DigiBtns_Click;
+            // b.Click 是「事件」, 按鈕被點擊的通知
+            // += 是「訂閱事件」的寫法 //-= 是「取消訂閱」的寫法
+            // digiBtns_Click 是「方法」, 當事件被觸發時會執行的方法
+
+            btnDot.Click += BtnDot_Click;
+            // btnDot 有獨立規則（不能重複）,所以另外寫一個事件處理方法
+
+            btnConvert.Click += BtnConvert_Click;
+            btnClear.Click += BtnClear_Click;
+
+
+            rbC2F.Checked = true; // 預設選擇攝氏轉華氏
+            // RadioButton 的 Checked 屬性表示該按鈕是否被選取
+            // 型別是 bool (true:這個選項被勾選/false:沒被選)
+            // 因為 RadioButton 有互斥機制，所以只需要設定其中一個 Checked 屬性為 true 
+            label1.Text = "結果";
+        }
+
+        // 把數字貼到輸入框最後面
+        private void DigiBtns_Click(object sender, EventArgs e)
+        {
+            var btn = (Button)sender; // 將 sender 轉型為 Button 類型
+            AppendToInput(btn.Text); // 將按鈕的文字內容傳給 AppendToInput 方法
+        }
+
+        private void BtnDot_Click(object sender, EventArgs e)
+        {
+            
+            if (string.IsNullOrWhiteSpace(txtInput.Text))
+            {
+                AppendToInput("0.");
+            } 
+            else if (!txtInput.Text.Contains("."))
+            {
+                // 如果目前還沒有小數點，才可以加一個「.」
+                AppendToInput(".");
+            }
+        }
+
+        private void BtnConvert_Click(object sender, EventArgs e)
+        {
+            //「如果沒有成功轉成數字」的話（代表輸入錯誤），那就執行 { ... }裡的內容
+            // 不是數字的話 = TryGetInput 回傳 false, !TryGetInput => !false = true
+            if (!TryGetInput(out double value))
+            {
+                MessageBox.Show("請輸入正確的數字（可含小數或負號）。", "格式錯誤");
+                txtInput.Focus();
+                txtInput.SelectAll();
+                return;
+            }
+
+            if (rbC2F.Checked)
+            {
+                double f = C2F(value);
+                label1.Text = $"結果：{value}°C = {f:F1}°F";
+                // F1 代表保留 1 位小數
+            }
+            else // rbF2C
+            {
+                double c = F2C(value);
+                label1.Text = $"結果：{value}°F = {c:F1}°C";
+            }
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            txtInput.Clear();
+            label1.Text = "結果：";
+            txtInput.Focus();
+        }
+
+        //每個按鈕都有一個屬性 .Text，就是顯示在按鈕上的字
+        // btn1.Text = "1"
+        private void AppendToInput(string text)
+        {
+            // 避免前導 0 問題：若目前是 "0" 且不是輸入小數點，改成新字元
+            if (txtInput.Text == "0" && text != ".")
+            {
+                txtInput.Text = text;
+            }
+            else
+            {
+                txtInput.AppendText(text);
+            }
+            txtInput.Focus(); // 將焦點回到輸入框，方便繼續輸入
+            txtInput.SelectionStart = txtInput.Text.Length; // 將游標移到文字最後
+        }
+
+        //確認輸入的文字能不能轉成數字
+        private bool TryGetInput(out double value)
+        {
+            return double.TryParse(txtInput.Text, out value);
+            //out 是 C# 的一種參數傳遞方式, 利用方法把一個值回傳到外面去
+            //value 是一個「要讓方法幫你填入結果」的變數
+        }
+        private static double C2F(double c)
+        {
+            return c * 9 / 5 + 32;
+        }
+        private static double F2C(double f)
+        {
+            return (f - 32) * 5 / 9;
         }
     }
 }
